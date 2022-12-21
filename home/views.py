fcfbfd87f56198a9c4ef69eff21ec98b3a48a191
unknown <muhammmajameel@gmail.com>
2022-12-21 17:34:31 +0500

    first commit

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("this is my home page")
+
+def  abount(req):
+    return HttpResponse("this is my about page")
